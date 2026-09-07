f7116755cb721fe0ab9aa55f605c43fddb779f85
Mtzallqmy <mtzallqmy@gmail.com>
2026-09-07 06:39:38 +0300

    fix: remove unused learning import

--- apps/mobile/lib/src/screens/learning.dart
+++ apps/mobile/lib/src/screens/learning.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../learning.dart';
-import '../models.dart';
 import '../quran_audio.dart';
 import '../quran_models.dart';
 import '../services.dart';

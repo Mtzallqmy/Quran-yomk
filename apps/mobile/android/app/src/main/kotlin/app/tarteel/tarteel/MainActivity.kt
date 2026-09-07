b1c9a502d65231fc2bb85b153e927723d932d3c0
Mtzallqmy <mtzallqmy@gmail.com>
2026-09-07 07:28:10 +0300

    fix: restore Android notification permission and registration (#56)
    * fix: make Android push permission registration reliable
    
    * fix: make Android push permission registration reliable
    
    * fix: make Android push permission registration reliable
    
    * fix: make Android push permission registration reliable
    
    * fix: make Android push permission registration reliable
    
    * style: format push notification tests
    
    * fix: disable backend delivery when Android permission is revoked

--- apps/mobile/android/app/src/main/kotlin/app/tarteel/tarteel/MainActivity.kt
+++ apps/mobile/android/app/src/main/kotlin/app/tarteel/tarteel/MainActivity.kt
@@ -2,11 +2,11 @@ package app.tarteel.tarteel
 
 import android.content.Intent
 import android.provider.Settings
-import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity() {
+class MainActivity : AudioServiceActivity() {
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         MethodChannel(
